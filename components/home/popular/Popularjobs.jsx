5c465b4e986bb293e06517a03fb93d42eb086b54
SyedKamranGH <syed.kam.ahmed@gmail.com>
2025-01-28 02:58:46 +0500

    Feature: Created useFetch hook

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -10,11 +10,20 @@ import { useRouter } from "expo-router";
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React Developer",
+    num_pages: 1,
+  });
+
+  console.log("Data ---> ", data);
+
+  // const isLoading = false;
+  // const error = false;
 
   return (
     <View style={styles.container}>
---
+++ hook/useFetch.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { RAPID_API_KEY } from "@env";
+
+const rapidAoiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint = "company-job-salary", query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    params: {
+      ...query,
+    },
+    headers: {
+      "x-rapidapi-key": rapidAoiKey,
+      "x-rapidapi-host": "jsearch.p.rapidapi.com",
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  //   company: "Amazon",
+  //   job_title: "software developer",
+  //   location_type: "ANY",
+  //   years_of_experience: "ALL",
+
+  // try {
+  // 	const response = await axios.request(options);
+  // 	console.log(response.data);
+  // } catch (error) {
+  // 	console.error(error);
+  // }
+
+  return { data, isLoading, error, refetch };
+};

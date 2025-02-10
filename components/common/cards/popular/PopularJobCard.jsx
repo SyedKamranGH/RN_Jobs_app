9acabb9e62e01115c4e77271f17811a274ebda69
Syed Kamran Ahmed <108738456+SyedKamranGH@users.noreply.github.com>
2025-02-10 16:27:29 +0500

    Feature/rnj 0002 popular jobs page (#4)
    * Feature: Include imports
    
    * Feature: Popular Job Basic UI model
    
    * Feature: Created useFetch hook
    
    * Fix: env import
    
    * Fix: Data fetching and popular job page
    
    * Feature: Popular Job Card UI
    
    * Fix: Null images and comment out checkImageURL

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+  Image,
+} from "react-native";
+// import { checkImageURL } from "../../../../utils";
+import styles from "./popularjobcard.style";
 
-import styles from './popularjobcard.style'
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
+  const dummyImageUrl =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP2YDRyiv2-xSlBBMkU-EBSr2C5VBliRwxsQ&s";
 
-const PopularJobCard = () => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      {/* {console.log("item ----> ", !item?.employer_logo, item?.employer_logo)} */}
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{
+            uri: !item?.employer_logo ? dummyImageUrl : item?.employer_logo,
+          }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item?.employer_name}
+      </Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <Text style={styles.location}>{item.job_country}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-export default PopularJobCard
+export default PopularJobCard;
---
+++ hook/useFetch.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    params: {
+      ...query,
+    },
+    headers: {
+      "x-rapidapi-key": "f03c4b7dbemshd609389dbfb7453p194b1cjsn0310b15e7187",
+      "x-rapidapi-host": "jsearch.p.rapidapi.com",
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
